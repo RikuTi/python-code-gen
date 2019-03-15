5ff0024a083acddfd7dbb349b6a8fc014e5168fc
RikuTi <31843957+RikuTi@users.noreply.github.com>
2019-03-15 16:14:47 +0200

    Added generated output

---
+++ codegen.cpp
@@ -0,0 +1,118 @@
+/*python_class_gen_begin*/
+class python_class_0
+{
+public:
+	python_class_0()
+	{
+		python_class_variable_0 = 1814;
+		python_class_variable_1 = "nCoAik";
+		python_class_variable_2 = false;
+		python_class_variable_3 = 2553;
+		python_class_variable_4 = 8654.49;
+	}
+	int& get_python_class_variable_0()
+	{
+		return python_class_variable_0;
+	}
+
+	std::string& get_python_class_variable_1()
+	{
+		return python_class_variable_1;
+	}
+
+	bool& get_python_class_variable_2()
+	{
+		return python_class_variable_2;
+	}
+
+	int& get_python_class_variable_3()
+	{
+		return python_class_variable_3;
+	}
+
+	float& get_python_class_variable_4()
+	{
+		return python_class_variable_4;
+	}
+
+private:
+	int python_class_variable_0;
+	std::string python_class_variable_1;
+	bool python_class_variable_2;
+	int python_class_variable_3;
+	float python_class_variable_4;
+};
+/*python_class_gen_end*/
+/*python_class_gen_begin*/
+class python_class_1
+{
+public:
+	python_class_1()
+	{
+		python_class_variable_0 = 1314;
+		python_class_variable_1 = false;
+		python_class_variable_2 = false;
+		python_class_variable_3 = 2805.7;
+		python_class_variable_4 = "i1d5N8";
+		python_class_variable_5 = 7203.2;
+		python_class_variable_6 = "4WZVGe";
+		python_class_variable_7 = 7250;
+		python_class_variable_8 = 1687;
+	}
+	int& get_python_class_variable_0()
+	{
+		return python_class_variable_0;
+	}
+
+	bool& get_python_class_variable_1()
+	{
+		return python_class_variable_1;
+	}
+
+	bool& get_python_class_variable_2()
+	{
+		return python_class_variable_2;
+	}
+
+	float& get_python_class_variable_3()
+	{
+		return python_class_variable_3;
+	}
+
+	std::string& get_python_class_variable_4()
+	{
+		return python_class_variable_4;
+	}
+
+	float& get_python_class_variable_5()
+	{
+		return python_class_variable_5;
+	}
+
+	std::string& get_python_class_variable_6()
+	{
+		return python_class_variable_6;
+	}
+
+	int& get_python_class_variable_7()
+	{
+		return python_class_variable_7;
+	}
+
+	int& get_python_class_variable_8()
+	{
+		return python_class_variable_8;
+	}
+
+private:
+	int python_class_variable_0;
+	bool python_class_variable_1;
+	bool python_class_variable_2;
+	float python_class_variable_3;
+	std::string python_class_variable_4;
+	float python_class_variable_5;
+	std::string python_class_variable_6;
+	int python_class_variable_7;
+	int python_class_variable_8;
+};
+/*python_class_gen_end*/
